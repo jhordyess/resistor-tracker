f4d3f7ad1fe97be8a3d5068147df806fbe13f14e
Jhordy Gavinchu <dev@jhordyess.com>
2023-04-18 21:57:44 +0000

    Updated bundle scripts ✔ Added new alias

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,6 +14,7 @@ module.exports = {
     alias: {
       "@components": path.resolve(__dirname, "src", "components"),
       "@pages": path.resolve(__dirname, "src", "pages"),
+      "@utils": path.resolve(__dirname, "src", "utils"),
     },
   },
   module: {
