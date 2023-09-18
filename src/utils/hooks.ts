b1127b856de323f8ea3ab5f9e7bbf8475f9b2ff5
jhordyess <dev@jhordyess.com>
2023-09-18 19:59:24 +0000

    Enhanced JSON parse

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 
+const parseJSON = (key: string) => {
+  const value = localStorage.getItem(key);
+  return value ? JSON.parse(value) : null;
+};
+
 export function useSemiPersistentState<S>(
   key: string,
   initialState: S
 ): [S, React.Dispatch<React.SetStateAction<S>>] {
-  const [value, setValue] = useState<S>(
-    JSON.parse(localStorage.getItem(key) || "") || initialState
-  );
+  const [value, setValue] = useState<S>(parseJSON(key) || initialState);
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
-  }, [value, key]); //? key is added to the dependency array to avoid the warning?
+  }, [value, key]);
 
   return [value, setValue];
 }
